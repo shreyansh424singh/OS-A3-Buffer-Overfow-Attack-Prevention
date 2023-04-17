563b04b74266252d3a3dcbefb048dd7cd0ef1923
shreyansh424singh <shrey424cs@gmail.com>
2023-04-17 14:24:31 +0530

    buffer overflow file

---
+++ xv6-public/buffer_overflow.c
@@ -0,0 +1,30 @@
+#include "types.h"
+#include "user.h"
+#include "fcntl.h"
+
+void foo()
+{
+    printf(1, "SECRET_STRING");
+}
+
+
+void vulnerable_func(char *payload)
+{
+    char buffer[4];
+    strcpy(buffer, payload);
+}
+
+int main(int argc, char *argv[])
+{
+    int fd;
+
+    fd = open("payload", O_RDONLY);
+    char payload[100];
+
+    read(fd, payload, 100);
+
+    vulnerable_func(payload);
+
+    close(fd);
+    exit();
+}
